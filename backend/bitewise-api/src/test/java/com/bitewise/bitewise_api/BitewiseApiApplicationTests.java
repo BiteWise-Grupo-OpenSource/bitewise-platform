faaf33fc8e5bc117eef743cb2065f93fb4a1c169
malumunayco <malumunaycoa05@gmail.com>
2026-06-19 01:30:41 -0500

    feat(backend): initialize Spring Boot project

---
+++ backend/bitewise-api/src/test/java/com/bitewise/bitewise_api/BitewiseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitewise.bitewise_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitewiseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
